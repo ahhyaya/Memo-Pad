544a79e91184614ae2af5129afd110afa91921ba
doldol123 <toruig59@gmail.com>
2022-10-26 19:52:25 -0700

    change get single note function after todays class

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 var fs = require('fs')
 const savedNotes = require('./db/db.json')
+const path = require('path');
+const { execArgv } = require('process');
 
 const app = express();
 const PORT = 3001;
-const path = require('path');
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-// app.get('/', (req, res) => res.send('Static assets!'));
+app.get('/', (req, res) => res.send(path.join(__dirname, '/public/index.html')));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -18,43 +21,66 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.json(savedNotes)
-);
+// Get request for a single note
+app.get('/api/notes/:id/', (req, res) => {
+  if(req.body && req.params.id) {
+    console.info(`${req.method} request received to get a single note`);
+    const noteId = req.params.id;
+    for (let i = 0; i < savedNotes.length; i++) {
+      const currentNote = savedNotes[i];
+      if(currentNote.id === noteId) {
+        res.json(currentNote);
+        return;
+      }
+    }
+    res.json('Note ID not found');
+  }
+}
 
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  console.log(newNote);
-  savedNotes.push(newNote);
-  res.json(newNote);
+
+
+
+
+
+
+app.post('/api/notes', (req, res, next) => {
+  const newNote = new savedNotes({
+    header: req.headers,
+    body: req.body
+  });
+  savedNotes.push(newNote).then(
+    () => {
+      res.status(201).json({
+        message: 'Note saved!'
+      });
+    }
+  ).catch(
+    (err) => {
+      res.status(400).json({
+        err:err
+      })
+    }
+  )
 })
 
 // app.delete('/api/notes', (req, res) => {
 //   res.send(savedNotes)
 // })
 
-// const handleRequest = (req, res) => {
-//   fs.readFile(`${__dirname}/notes.html`, (err,data) => {
-//     if(err) throw err;
-//     res.writeHead(200, {'content-type': 'text/html'});
-//     res.end(data)
-//   });
-// };
+const handleRequest = (req, res) => {
+  fs.readFile(`${__dirname}/notes.html`, (err,data) => {
+    if(err) throw err;
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(data)
+  });
+};
 
 
 // app.put('api/notes', (req, res) => {
 //   res.send(savedNotes)
 // })
 
-app.get('/api/notes/:id/', (req, res) => {
-  const noteId = req.params.id.toLowerCase();
-  // for (let i = 0; i < savedNotes.length; i++) {
-  // if(noteId === savedNotes[i].note.toLowerCase()){
-  return res.json(savedNotes[i]);
-//   }
-// }
-//   return res.json('No match note found');
-});
+
 
 
 
